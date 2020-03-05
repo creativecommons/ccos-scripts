c2041a0c35861e8a4a93fa11ad96ece1f1fa2c5a
shubham <shubhampcpandey13@gmail.com>
2020-03-06 03:27:28 +0530

    Lint and fix errors in generate_projects_page

--- generate_projects_page/generate_projects_page.py
+++ generate_projects_page/generate_projects_page.py
@@ -1,4 +1,3 @@
-import datetime
 import json
 import os
 from base64 import b64decode
@@ -11,29 +10,32 @@ from github.GithubException import UnknownObjectException
 from cryptography.fernet import Fernet
 
 
-GIT_USER_NAME = 'CC creativecommons.github.io Bot'
-GIT_USER_EMAIL = 'cc-creativecommons-github-io-bot@creativecommons.org'
+GIT_USER_NAME = "CC creativecommons.github.io Bot"
+GIT_USER_EMAIL = "cc-creativecommons-github-io-bot@creativecommons.org"
 
-GITHUB_USERNAME = 'cc-creativecommons-github-io-bot'
-GITHUB_ORGANIZATION = 'creativecommons'
-GITHUB_REPO_NAME = 'creativecommons.github.io-source'
-ENCRYPTED_GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-# Decrypt code should run once and variables stored outside of the function
-# handler so that these are decrypted once per container
-DECRYPTED_GITHUB_TOKEN = decrypt(ENCRYPTED_GITHUB_TOKEN)
-GITHUB_REPO_URL_WITH_CREDENTIALS = f'https://{GITHUB_USERNAME}:{DECRYPTED_GITHUB_TOKEN}@github.com/{GITHUB_ORGANIZATION}/{GITHUB_REPO_NAME}.git'
-CC_METADATA_FILE_NAME = '.cc-metadata.yml'
+GITHUB_USERNAME = "cc-creativecommons-github-io-bot"
+GITHUB_ORGANIZATION = "creativecommons"
+GITHUB_REPO_NAME = "creativecommons.github.io-source"
 
-LAMBDA_WORKING_DIRECTORY = '/tmp'
-GIT_WORKING_DIRECTORY = f'{LAMBDA_WORKING_DIRECTORY}/{GITHUB_REPO_NAME}'
-JSON_FILE_DIRECTORY = f'{GIT_WORKING_DIRECTORY}/databags'
 
 def decrypt(secret):
-    key_file = open('key_file', 'rb')
+    key_file = open("key_file", "rb")
     key = key_file.read()
     key_file.close()
     cipher_suite = Fernet(key)
-    return cipher_suite.decrypt(secret)
+    return cipher_suite.decrypt(b64decode(secret))
+
+
+ENCRYPTED_ADMIN_GITHUB_TOKEN = os.environ["ADMIN_GITHUB_TOKEN"]
+# Decrypt code should run once and variables stored outside of the function
+# handler so that these are decrypted once per container
+DECRYPTED_ADMIN_GITHUB_TOKEN = decrypt(ENCRYPTED_ADMIN_GITHUB_TOKEN)
+GITHUB_REPO_URL_WITH_CREDENTIALS = f"https://{GITHUB_USERNAME}:{DECRYPTED_ADMIN_GITHUB_TOKEN}@github.com/{GITHUB_ORGANIZATION}/{GITHUB_REPO_NAME}.git"
+CC_METADATA_FILE_NAME = ".cc-metadata.yml"
+
+LAMBDA_WORKING_DIRECTORY = "/tmp"
+GIT_WORKING_DIRECTORY = f"{LAMBDA_WORKING_DIRECTORY}/{GITHUB_REPO_NAME}"
+JSON_FILE_DIRECTORY = f"{GIT_WORKING_DIRECTORY}/databags"
 
 
 def set_up_repo():
@@ -41,24 +43,24 @@ def set_up_repo():
     if not os.path.isdir(GIT_WORKING_DIRECTORY):
         git.Repo.clone_from(url=GITHUB_REPO_URL_WITH_CREDENTIALS, to_path=".")
     git.Remote.pull(GITHUB_REPO_URL_WITH_CREDENTIALS)
-    return f'{LAMBDA_WORKING_DIRECTORY}/{GITHUB_REPO_NAME}'
+    return f"{LAMBDA_WORKING_DIRECTORY}/{GITHUB_REPO_NAME}"
 
 
 def set_up_git_user():
-    os.environ['GIT_AUTHOR_NAME'] = GIT_USER_NAME
-    os.environ['GIT_AUTHOR_EMAIL'] = GIT_USER_EMAIL
-    os.environ['GIT_COMMITTER_NAME'] = GIT_USER_NAME
-    os.environ['GIT_COMMITTER_EMAIL'] = GIT_USER_EMAIL
+    os.environ["GIT_AUTHOR_NAME"] = GIT_USER_NAME
+    os.environ["GIT_AUTHOR_EMAIL"] = GIT_USER_EMAIL
+    os.environ["GIT_COMMITTER_NAME"] = GIT_USER_NAME
+    os.environ["GIT_COMMITTER_EMAIL"] = GIT_USER_EMAIL
 
 
 def set_up_github_client():
-    github_client = Github(DECRYPTED_GITHUB_TOKEN)
+    github_client = Github(DECRYPTED_ADMIN_GITHUB_TOKEN)
     return github_client
 
 
 def get_cc_organization(github_client):
-     cc = github_client.get_organization(GITHUB_ORGANIZATION)
-     return cc
+    cc = github_client.get_organization(GITHUB_ORGANIZATION)
+    return cc
 
 
 def get_repositories(organization):
@@ -68,25 +70,25 @@ def get_repositories(organization):
 
 def get_repo_github_data(repo):
     repo_github_data = {
-        'id': repo.id,
-        'name': repo.name,
-        'url': repo.html_url,
-        'description': emoji.emojize(repo.description),
-        'website': repo.homepage,
-        'language': repo.language,
-        'created': repo.created_at.isoformat(),
+        "id": repo.id,
+        "name": repo.name,
+        "url": repo.html_url,
+        "description": emoji.emojize(repo.description),
+        "website": repo.homepage,
+        "language": repo.language,
+        "created": repo.created_at.isoformat(),
     }
     try:
         license = repo.get_license()
     except UnknownObjectException:
         license = None
     if license:
-        repo_github_data['license'] = {
-            'name': license.license.name,
-            'url': license.html_url
+        repo_github_data["license"] = {
+            "name": license.license.name,
+            "url": license.html_url,
         }
     else:
-        repo_github_data['license'] = None
+        repo_github_data["license"] = None
     return repo_github_data
 
 
@@ -96,8 +98,11 @@ def get_repo_cc_metadata(repo):
     except UnknownObjectException:
         return {}
     cc_metadata = yaml.load(cc_metadata_file.decoded_content)
-    if 'technologies' in cc_metadata:
-        cc_metadata['technologies'] = [technology.strip() for technology in cc_metadata['technologies'].split(',')]
+    if "technologies" in cc_metadata:
+        cc_metadata["technologies"] = [
+            technology.strip()
+            for technology in cc_metadata["technologies"].split(",")
+        ]
     return cc_metadata
 
 
@@ -107,30 +112,30 @@ def get_repo_data_list(repos):
     total = repos.totalCount
 
     for repo in repos:
-        print(f'Processing {count} of {total} – {repo.name}')
+        print(f"Processing {count} of {total} – {repo.name}")
         if not repo.private:
             repo_cc_metadata = get_repo_cc_metadata(repo)
-            is_engineering_project = repo_cc_metadata.get('engineering_project', True)
+            is_engineering_project = repo_cc_metadata.get(
+                "engineering_project", True
+            )
             if is_engineering_project:
                 repo_github_data = get_repo_github_data(repo)
                 repo_data = {**repo_github_data, **repo_cc_metadata}
                 repo_data_list.append(repo_data)
             else:
-                print('\tNot an active engineering project, skipping')
+                print("\tNot an active engineering project, skipping")
         count += 1
-    return sorted(repo_data_list, key=lambda k: k['name'].lower()) 
+    return sorted(repo_data_list, key=lambda k: k["name"].lower())
 
 
 def get_repo_data_dict(repo_data_list):
     # This is needed because Lektor needs a top level object (not array) in the JSON file.
-    return {
-        'repos': repo_data_list
-    }
+    return {"repos": repo_data_list}
 
 
 def generate_json_file(repo_data_dict):
-    json_filename = f'{JSON_FILE_DIRECTORY}/repos.json'
-    with open(json_filename, 'w') as json_file:
+    json_filename = f"{JSON_FILE_DIRECTORY}/repos.json"
+    with open(json_filename, "w") as json_file:
         json.dump(repo_data_dict, json_file, sort_keys=True, indent=4)
     return json_filename
 
@@ -143,10 +148,10 @@ def commit_and_push_changes(json_filename):
     # branch_name = f'{now}_sync'
     # git.exec_command('checkout', f'-b{branch_name}', cwd=GIT_WORKING_DIRECTORY)
     git_diff = git.IndexFile.diff(GIT_WORKING_DIRECTORY)
-    if git_diff != (b'', b''):
+    if git_diff != (b"", b""):
         repo = git.Repo(GIT_WORKING_DIRECTORY)
-        repo.index.add(items=f'{json_filename}')
-        repo.index.commit(message='Syncing new repository changes.')
+        repo.index.add(items=f"{json_filename}")
+        repo.index.commit(message="Syncing new repository changes.")
         git.Remote.push(GITHUB_REPO_URL_WITH_CREDENTIALS)
 
 
