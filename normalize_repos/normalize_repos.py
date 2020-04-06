8aaf07b0739e6cb466e29ad7f39d98098cc819bd
SaurabhAgarwala <saur.agarwala@gmail.com>
2020-04-06 23:48:17 +0530

    Define constants in a separate file

--- normalize_repos/normalize_repos.py
+++ normalize_repos/normalize_repos.py
@@ -4,70 +4,16 @@
 This script ensures that all active repositories in the creativecommons GitHub
 organization are consistent. Please see README.md.
 """
-# Standard library
-import os
-
 # Third-party
 from github import Github
 
 # Local/library specific
 from labels import REQUIRED_LABELS
-
-
-BRANCH_PROTECTION_EXEMPT_REPOSITORIES = [
-    # non-engineering repo
-    "australian-chapter",
-    # non-engineering repo
-    "cc-cert-core",
-    # non-engineering repo
-    "cc-cert-edu",
-    # non-engineering repo
-    "cc-cert-gov",
-    # non-engineering repo
-    "cc-cert-lib",
-    # exempted to allow transifex updates
-    "cc.i18n",
-    # exempted to allow community maintainer to self-merge PRs
-    "ccsearch-browser-extension",
-    # exempted for bot pushes to master
-    "creativecommons.github.io-source",
-    # exempted for bot pushes to master
-    "creativecommons.github.io",
-    # non-engineering repo
-    "global-network-strategy",
-    # non-engineering repo
-    "network-platforms",
-    # non-engineering repo
-    "sre-wiki-js",
-    # non-engineering repo
-    "tech-support",
-]
-
-BRANCH_PROTECTION_REQUIRED_STATUS_CHECK_MAP = {
-    "cccatalog-api": ["continuous-integration/travis-ci"],
-    "cccatalog-frontend": ["ci/circleci: lint", "ci/circleci: unit"],
-    "creativecommons.github.io-source": ["continuous-integration/travis-ci"],
-    "fonts": [
-        "ci/circleci: lint",
-        "ci/circleci: build",
-        "netlify/cc-fonts/deploy-preview",
-    ],
-    "vocabulary": [
-        "ci/circleci: lint",
-        "ci/circleci: test",
-        "ci/circleci: build",
-        "netlify/cc-vocabulary/deploy-preview",
-    ],
-    "vue-vocabulary": [
-        "ci/circleci: lint",
-        "ci/circleci: unit",
-        "netlify/cc-vue-vocabulary/deploy-preview",
-    ],
-}
+from constants import ADMIN_GITHUB_TOKEN, BRANCH_PROTECTION_EXEMPT_REPOSITORIES, \
+    BRANCH_PROTECTION_REQUIRED_STATUS_CHECK_MAP
 
 
 def set_up_github_client():
-    ADMIN_GITHUB_TOKEN = os.environ["ADMIN_GITHUB_TOKEN"]
     github = Github(ADMIN_GITHUB_TOKEN)
     return github
 
