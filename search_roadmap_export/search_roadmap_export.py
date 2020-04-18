5c2203c05473133b9183b4b9a6dcb16b1eca1c68
Ari <akmadian@gmail.com>
2020-04-17 17:26:45 -0700

    Add support for getting public description, modify filtering approach

--- search_roadmap_export/search_roadmap_export.py
+++ search_roadmap_export/search_roadmap_export.py
@@ -1,5 +1,6 @@
 # Standard Library
 import os
+import json
 
 # Third-party
 import asana
@@ -9,12 +10,12 @@ from config import CONFIG
 
 ASANA_CLIENT = asana.Client.access_token(os.environ["ASANA_TOKEN"])
 
-"""
+
 def get_cc_asana_workspace():
     for workspace in ASANA_CLIENT.workspaces.find_all():
         if workspace['name'] == 'creativecommons.org':
-            return workspace
-
+            return workspace['gid']
+"""
 def get_search_roadmap_project(workspace):
     for project in ASANA_CLIENT.projects.find_by_workspace(
         workspace['gid'],
@@ -27,13 +28,49 @@ workspace = get_cc_asana_workspace()
 project = get_search_roadmap_project(workspace)
 print(project['gid'])
 """
-def tasks_by_section():
-    for section_name, section_gid in CONFIG['ROADMAP_SECTIONS'].items():
-        tasks = ASANA_CLIENT.tasks.find_by_section(CONFIG['ROADMAP_SECTIONS'][section_name])
-
 
 """
+{
+    gid: name
+}
+"""
+def tasks_by_section():
+    sections = {
+        'Q12020': [],
+        'Q22020': [],
+        'Q32020': [],
+        'Q42020': []
+    }
 
+
+    for section_name, section_gid in CONFIG['ROADMAP_SECTIONS'].items(): # for section in included sections
+        tasks = ASANA_CLIENT.tasks.find_by_section(
+            CONFIG['ROADMAP_SECTIONS'][section_name],
+            opt_fields=['name', 'custom_fields', 'tags.gid']
+        )
+        for task in tasks:
+            if not has_ignore_tag(task):
+                print(task)
+                sections[section_name].append({
+                    'gid': task['gid'],
+                    'name': task['name'],
+                    'public_description': get_public_description(task)
+                })
+
+    return sections
+
+def has_ignore_tag(task):
+    for tag in task['tags']:
+        return tag['gid'] == CONFIG['ROADMAP_IGNORE_TAG_GID']
+
+def get_public_description(task):
+    custfields = task['custom_fields']
+    for field in custfields:
+        if field['name'] == 'Public Description':
+            return field['text_value']
+
+"""
+Returns a list of task gids, each for a task tagged with the 'roadmap_public' tag.
 """
 def tasks_with_tag():
     return list(map(
@@ -42,11 +79,23 @@ def tasks_with_tag():
     ))
 
 
-def filter_tasks_without_tag(all_in_sections, all_with_tag):
-    pass
+def filter_tasks_without_tag(tasks_by_section, tasks_with_tag):
+    sections = {
+        'Q12020': [],
+        'Q22020': [],
+        'Q32020': [],
+        'Q42020': []
+    }
+
+    for section, tasks in tasks_by_section.items():
+        for task in tasks:
+            if task['gid'] in tasks_with_tag:
+                sections[section].append(task)
+
+    return sections
 
 
-
+"""
 tasks = ASANA_CLIENT.tags.get_tasks_with_tag(CONFIG['ROADMAP_PUBLIC_TAG_GID'])
 for task in tasks:
     print(task)
@@ -54,3 +103,22 @@ for task in tasks:
 sections = ASANA_CLIENT.sections.find_by_project(CONFIG['ROADMAP_PROJECT_GID'])
 for section in sections:
     print(section)
+
+
+customfields = ASANA_CLIENT.custom_fields.find_by_workspace(
+    CONFIG['CC_WORKSPACE_GID']
+)
+for field in customfields:
+    print(field)
+"""
+
+sections = tasks_by_section()
+
+print(json.dumps(sections))
+
+"""
+for k, v in filtered.items():
+    print(k)
+    for gid, name in v.items():
+        print('\t{}: {}'.format(gid, name))
+        """
