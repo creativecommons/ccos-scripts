65191ca09b03b9fde04bf43500a3417cf8bbd4d4
Dhruv Bhanushali <dhruv_b@live.com>
2020-08-16 02:35:02 +0530

    Store GitHub username with other related constants

--- sync_community_team/sync_teams.py
+++ sync_community_team/sync_teams.py
@@ -3,6 +3,8 @@
 # Local
 from get_community_team_data import get_community_team_data
 from set_teams_on_github import create_teams_for_data
+from set_codeowners import create_codeowners_for_data
 
 if __name__ == "__main__":
     create_teams_for_data(get_community_team_data())
+    create_codeowners_for_data(get_community_team_data())
