7195c73eb0098b5b7ff480923a76bd27723811f6
Timid Robot Zehta <timid@creativecommons.org>
2022-04-08 14:30:28 -0700

    reorganize sync_community_teams and add logging

--- ccos/gh_utils.py
+++ ccos/gh_utils.py
@@ -2,6 +2,7 @@
 import inspect
 import logging
 import os
+import re
 import sys
 
 # Third-party
@@ -11,13 +12,13 @@ from github.GithubException import BadCredentialsException
 # First-party/Local
 from ccos import log
 
-log_name = os.path.basename(os.path.splitext(inspect.stack()[-1].filename)[0])
-logger = logging.getLogger(log_name)
-log.reset_handler()
-
 GITHUB_ORGANIZATION = "creativecommons"
 GITHUB_USERNAME = "cc-creativecommons-github-io-bot"
 GITHUB_TOKEN = None
+
+log_name = os.path.basename(os.path.splitext(inspect.stack()[-1].filename)[0])
+logger = logging.getLogger(log_name)
+log.reset_handler()
 
 
 def set_up_github_client():
@@ -45,3 +46,55 @@ def get_cc_organization(github_client):
         sys.exit(1)
     logger.log(log.SUCCESS, "done.")
     return cc
+
+
+def get_team_slug_name(project_name, role):
+    """
+    Get the team name and team slug based on GitHub's naming scheme. By
+    convention, teams are named in a well-defined .
+
+    team name schema
+    CT: <project name> <role, pluralized>
+
+    team slug schema
+    ct-<project_name_slug>-<role_slug>
+
+    @param project_name: the name of the project to which the team belongs
+    @param role: the role held by folks in the team
+    @return: the slug and name of the team
+    """
+    sanitized_role = pluralized(role).replace("Project ", "")
+    team_name = f"CT: {project_name} {sanitized_role}"
+    team_slug = slugified(team_name)
+    return team_slug, team_name
+
+
+def pluralized(word):
+    """
+    Get the plural of the given word. Contains a dictionary for non-standard
+    plural forms. If the word ends in one of 5 known endings, appends an 'es'
+    to the end. By default, just appends an 's' to the given word.
+
+    @param word: the word to pluralize
+    @return: the plural form of the noun
+    """
+    defined_plurals = {"person": "people"}
+    if word in defined_plurals:
+        return defined_plurals[word]
+
+    es_endings = ["s", "sh", "ch", "x", "z"]
+    if any([word.endswith(ending) for ending in es_endings]):
+        return f"{word}es"
+    else:
+        return f"{word}s"
+
+
+def slugified(text):
+    """
+    Get the slug generated from the given text. Replaces all non-alphanumeric
+    characters with hyphens. Coalesces successive hyphens into one.
+
+    @param text: the text to slugify
+    @return: the slug made from the given text
+    """
+    return re.sub("-+", "-", re.sub(r"\W", "-", text.lower()))
