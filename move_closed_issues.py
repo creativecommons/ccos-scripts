690e1f19e0d1158c7b4bb47ce6caa873deb4c065
Timid Robot Zehta <timid@creativecommons.org>
2022-10-18 09:42:55 -0700

    update to use local libraries and improve readability with functions

--- move_closed_issues.py
+++ move_closed_issues.py
@@ -3,14 +3,18 @@
 Move closed Issues out of Backlog and into Active Sprint: Done.
 """
 # Standard library
+import argparse
+import logging
 import os
 import sys
 import traceback
 
-# Third-party
-import github
+# First-party/Local
+from ccos import gh_utils, log
 
-GITHUB_TOKEN = os.environ["ADMIN_GITHUB_TOKEN"]
+log.set_up_logging()
+logger = logging.getLogger(os.path.basename(__file__))
+log.reset_handler()
 
 
 class ScriptError(Exception):
@@ -20,43 +24,76 @@ class ScriptError(Exception):
         super(ScriptError, self).__init__(message)
 
 
-def main():
+def setup():
+    """Instantiate and configure argparse and logging.
+
+    Return argsparse namespace.
+    """
+    ap = argparse.ArgumentParser(description=__doc__)
+    ap.add_argument(
+        "-n",
+        "--dryrun",
+        action="store_true",
+        help="dry run: do not make any changes",
+    )
+    args = ap.parse_args()
+    return args
+
+
+def get_cards(cc):
+    active = None
     backlog = None
-    active_sprint = None
     done = None
-    github_client = github.Github(GITHUB_TOKEN)
-    cc = github_client.get_organization("creativecommons")
 
     for project in cc.get_projects():
         if project.name == "Active Sprint":
-            active_sprint = project
+            active = project
         elif project.name == "Backlog":
             backlog = project
 
-    for column in active_sprint.get_columns():
+    for column in active.get_columns():
         if column.name == "Done":
             done = column
             break
 
+    return backlog, done
+
+
+def move_cards(args, github, backlog, done):
     for column in backlog.get_columns():
-        print(f"{backlog.name}: {column.name}")
+        logger.log(logging.INFO, f"{backlog.name}: {column.name}")
         for card in column.get_cards():
             if not card.content_url or "/issues/" not in card.content_url:
                 continue
             content = card.get_content(content_type="Issue")
             if content.state != "closed":
                 continue
-            print(f"    {content.title}")
+            logger.log(logging.INFO, f"  {content.title}")
             try:
-                done.create_card(content_id=content.id, content_type="Issue")
-                print(f"     -> added to Active Sprint: {done.name}")
+                if not args.dryrun:
+                    done.create_card(
+                        content_id=content.id, content_type="Issue"
+                    )
+                logger.log(
+                    logging.INFO,
+                    f"    -> added to Active Sprint: {done.name}",
+                )
             except github.GithubException as e:
                 if e.data["errors"][0]["message"] != (
                     "Project already has the associated issue"
                 ):
                     raise
-            card.delete()
-            print("     -> removed.")
+            if not args.dryrun:
+                card.delete()
+            logger.log(logging.INFO, "    -> removed.")
+
+
+def main():
+    args = setup()
+    github = gh_utils.set_up_github_client()
+    cc = gh_utils.get_cc_organization(github)
+    backlog, done = get_cards(cc)
+    move_cards(args, github, backlog, done)
 
 
 if __name__ == "__main__":
@@ -65,13 +102,14 @@ if __name__ == "__main__":
     except SystemExit as e:
         sys.exit(e.code)
     except KeyboardInterrupt:
-        print("INFO (130) Halted via KeyboardInterrupt.", file=sys.stderr)
+        logger.log(logging.INFO, "Halted via KeyboardInterrupt.")
         sys.exit(130)
     except ScriptError:
         error_type, error_value, error_traceback = sys.exc_info()
-        print("ERROR {}".format(error_value), file=sys.stderr)
+        logger.log(logging.CRITICAL, f"{error_value}")
         sys.exit(error_value.code)
     except Exception:
-        print("ERROR (1) Unhandled exception:", file=sys.stderr)
-        print(traceback.print_exc(), file=sys.stderr)
+        logger.log(
+            logging.ERROR, f"Unhandled exception: {traceback.format_exc()}"
+        )
         sys.exit(1)
