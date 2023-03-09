2477a7b9107b19a7efcea7252c817145eaf99de4
Timid Robot Zehta <timid@creativecommons.org>
2023-03-09 08:15:19 -0800

    improve path handlings, temp dir use, and replace global variables with args

--- ccos/data/push_data_via_git.py
+++ ccos/data/push_data_via_git.py
@@ -6,6 +6,7 @@ import inspect
 import json
 import logging
 import os
+import tempfile
 
 # Third-party
 import git
@@ -26,28 +27,25 @@ GITHUB_REPO_URL_WITH_CREDENTIALS = (
     f"@github.com/{GITHUB_ORGANIZATION}/{GITHUB_REPO_NAME}.git"
 )
 
-WORKING_DIRECTORY = "/tmp"
-GIT_WORKING_DIRECTORY = f"{WORKING_DIRECTORY}/{GITHUB_REPO_NAME}"
-JSON_FILE_DIRECTORY = f"{GIT_WORKING_DIRECTORY}/databags"
+JSON_FILE_DIR = "databags"
 
 log_name = os.path.basename(os.path.splitext(inspect.stack()[-1].filename)[0])
 logger = logging.getLogger(log_name)
 log.reset_handler()
 
 
-def set_up_repo():
-    if not os.path.isdir(GIT_WORKING_DIRECTORY):
+def set_up_repo(git_working_dir):
+    if not os.path.isdir(git_working_dir):
         logger.log(logging.INFO, "Cloning repo...")
         repo = git.Repo.clone_from(
-            url=GITHUB_REPO_URL_WITH_CREDENTIALS, to_path=GIT_WORKING_DIRECTORY
+            url=GITHUB_REPO_URL_WITH_CREDENTIALS, to_path=git_working_dir
         )
     else:
         logger.log(logging.INFO, "Setting up repo...")
-        repo = git.Repo(GIT_WORKING_DIRECTORY)
+        repo = git.Repo(git_working_dir)
     origin = repo.remotes.origin
     logger.log(logging.INFO, "Pulling latest code...")
     origin.pull()
-    return f"{WORKING_DIRECTORY}/{GITHUB_REPO_NAME}"
 
 
 def set_up_git_user():
@@ -58,16 +56,16 @@ def set_up_git_user():
     os.environ["GIT_COMMITTER_EMAIL"] = GIT_USER_EMAIL
 
 
-def generate_json_file(data, filename):
+def generate_json_file(git_working_dir, data, filename):
     logger.log(logging.INFO, "Generating JSON file...")
-    json_filename = f"{JSON_FILE_DIRECTORY}/{filename}"
+    json_filename = os.path.join(git_working_dir, JSON_FILE_DIR, filename)
     with open(json_filename, "w") as json_file:
         json.dump(data, json_file, sort_keys=True, indent=4)
     return json_filename
 
 
-def commit_and_push_changes(json_filename):
-    repo = git.Repo(GIT_WORKING_DIRECTORY)
+def commit_and_push_changes(git_working_dir, json_filename):
+    repo = git.Repo(git_working_dir)
     git_diff = repo.index.diff(None)
     if git_diff:
         repo.index.add(items=f"{json_filename}")
@@ -80,7 +78,9 @@ def commit_and_push_changes(json_filename):
 
 
 def push_data(data, filename):
-    set_up_repo()
-    set_up_git_user()
-    json_filename = generate_json_file(data, filename)
-    commit_and_push_changes(json_filename)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        git_working_dir = os.path.join(temp_dir, GITHUB_REPO_NAME)
+        set_up_repo(git_working_dir)
+        set_up_git_user()
+        json_filename = generate_json_file(git_working_dir, data, filename)
+        commit_and_push_changes(git_working_dir, json_filename)
