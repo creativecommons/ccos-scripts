56e74ac2b5f17b3f837079678c5bbc71a0b61f3a
Timid Robot Zehta <timid@creativecommons.org>
2023-03-10 10:12:26 -0800

    refactor set_codeowners
    - use tempfile.TemporaryDirectory() for more reliable behavior
    - properly use credentials using new ccos.gh_utils.get_credentials()
      function
    - reduce code duplication
      - set variables once and pass them
      - improve (re)use of PyGithub objects
    - remove extra whitespace from log messages

--- ccos/teams/set_codeowners.py
+++ ccos/teams/set_codeowners.py
@@ -4,8 +4,8 @@ import inspect
 import logging
 import os
 import os.path
-import shutil
 from pathlib import Path
+from tempfile import TemporaryDirectory
 
 # Third-party
 import git
@@ -14,17 +14,14 @@ import git
 from ccos import log
 from ccos.gh_utils import (
     GITHUB_ORGANIZATION,
-    GITHUB_TOKEN,
-    GITHUB_USERNAME,
     get_cc_organization,
+    get_credentials,
     set_up_github_client,
 )
 from ccos.teams.set_teams_on_github import map_role_to_team
 
 GIT_USER_NAME = "CC creativecommons.github.io Bot"
 GIT_USER_EMAIL = "cc-creativecommons-github-io-bot@creativecommons.org"
-
-WORKING_DIRECTORY = Path("/tmp").resolve()
 
 SYNC_BRANCH = "ct_codeowners"
 
@@ -35,9 +32,8 @@ log.reset_handler()
 
 def create_codeowners_for_data(databag):
     set_up_git_user()
-
-    client = set_up_github_client()
-    organization = get_cc_organization(client)
+    github_client = set_up_github_client()
+    organization = get_cc_organization(github_client)
 
     logging.log(logging.INFO, "Identifying and fixing CODEOWNER issues...")
     projects = databag["projects"]
@@ -59,8 +55,9 @@ def create_codeowners_for_data(databag):
 
         logging.log(logging.INFO, "Checking all projects...")
         repos = project["repos"]
-        for repo in repos:
-            check_and_fix_repo(organization, repo, teams)
+        with TemporaryDirectory() as temp_dir:
+            for repo_name in repos:
+                check_and_fix_repo(organization, repo_name, teams, temp_dir)
     logging.log(log.SUCCESS, "Done")
 
 
@@ -95,7 +92,7 @@ def get_teams(organization, project_name, roles):
     return [team for team in role_team_map.values() if team is not None]
 
 
-def check_and_fix_repo(organization, repo, teams):
+def check_and_fix_repo(organization, repo_name, teams, temp_dir):
     """
     Identify issues with the CODEOWNERS file and rectify them. Missing
     CODEOWNERS files will be created. Incomplete CODEOWNERS files will be
@@ -105,42 +102,43 @@ def check_and_fix_repo(organization, repo, teams):
     @param repo: the repo to which the CODEOWNERS file being modified belongs
     """
 
-    logging.log(logging.INFO, f"Checking and fixing {repo}...")
-    set_up_repo(repo)
+    logging.log(logging.INFO, f"Checking and fixing {repo_name}...")
+    repo_dir = os.path.join(temp_dir, repo_name)
+    gh_repo = organization.get_repo(repo_name)
+    clone_url = get_github_repo_url_with_credentials(repo_name)
+    local_repo = set_up_repo(clone_url, repo_dir)
+    codeowners_path = Path(os.path.join(repo_dir, ".github", "CODEOWNERS"))
     fix_required = False
 
-    if not is_codeowners_present(repo):
+    if not codeowners_path.exists() and codeowners_path.is_file():
         fix_required = True
-        codeowners_path = get_codeowners_path(repo)
-        logging.log(logging.INFO, " CODEOWNERS does not exist, creating...")
+        logging.log(logging.INFO, "CODEOWNERS does not exist, creating...")
         os.makedirs(codeowners_path.parent, exist_ok=True)
         open(codeowners_path, "a").close()
-        logging.log(log.SUCCESS, "                Done.")
+        logging.log(log.SUCCESS, "Done.")
 
-    team_mention_map = get_team_mention_map(repo, teams)
+    team_mention_map = get_team_mention_map(codeowners_path, teams)
     if not all(team_mention_map.values()):
         fix_required = True
         logging.log(logging.INFO, "CODEOWNERS is incomplete, populating...")
-        add_missing_teams(repo, team_mention_map)
-        logging.log(log.SUCCESS, "                Done.")
+        add_missing_teams(codeowners_path, team_mention_map)
+        logging.log(log.SUCCESS, "Done.")
 
     if fix_required:
         logging.log(logging.INFO, "Pushing to GitHub...")
-        branch_name = commit_and_push_changes(repo)
+        branch_name = commit_and_push_changes(local_repo, codeowners_path)
         logging.log(log.SUCCESS, f"Pushed to {branch_name}.")
 
         logging.log(logging.INFO, "Opening a PR...")
-        pr = create_pull_request(organization, repo, branch_name)
-        logging.log(log.SUCCESS, f"                PR at {pr.url}.")
+        pr = create_pull_request(gh_repo, branch_name)
+        logging.log(log.SUCCESS, f"PR at {pr.url}.")
 
-    logging.log(logging.INFO, "Deleting clone...")
-    shutil.rmtree(get_repo_path(repo))
     logging.log(log.SUCCESS, "Done.")
 
     logging.log(log.SUCCESS, "All is well.")
 
 
-def commit_and_push_changes(repo):
+def commit_and_push_changes(local_repo, codeowners_path):
     """
     Create a new branch and push the CODEOWNER to it. This branch will be named
     in a particular format.
@@ -148,36 +146,34 @@ def commit_and_push_changes(repo):
     codeowner branch schema
     ct_codeowners_<timestamp>
 
-    @param repo: the repo to which the CODEOWNERS file being modified belongs
+    @param local_repo: GitPython Repo instance for the repo to which the
+                       CODEOWNERS file being modified belongs
+    @param codeowners_path: the path of the CODEOWNERS file
     @return: the name of the branch to which the changes were pushed
     """
-    repo = git.Repo(get_repo_path(repo))
-
     timestamp = int(datetime.datetime.now().timestamp())
     branch_name = f"{SYNC_BRANCH}_{timestamp}"
-    repo.git.checkout("HEAD", b=branch_name)
+    local_repo.git.checkout("HEAD", b=branch_name)
 
-    repo.index.add(items=".github/CODEOWNERS")
-    repo.index.commit(message="Sync Community Team to CODEOWNERS")
+    local_repo.index.add(items=codeowners_path)
+    local_repo.index.commit(message="Sync Community Team(s) to CODEOWNERS")
 
-    origin = repo.remotes.origin
+    origin = local_repo.remotes.origin
     origin.push(f"{branch_name}:{branch_name}")
 
     return branch_name
 
 
-def create_pull_request(organization, repo, branch_name):
+def create_pull_request(gh_repo, branch_name):
     """
     Create a PR from the newly created branch to the base branch of the
     repository containing the required changes to the CODEOWNERS.
 
-    @param organization: the organization to which the repository belongs
-    @param repo: the repo to which the CODEOWNERS file being modified belongs
+    @param gh_repo: PyGithub Repository object
     @param branch_name: the name of the branch containing the CODEOWNERS
                         changes
     """
-    repo = organization.get_repo(repo)
-    return repo.create_pull(
+    return gh_repo.create_pull(
         title="Sync Community Team to CODEOWNERS",
         body=(
             "This _automated PR_ updates your CODEOWNERS file to mention all "
@@ -185,62 +181,42 @@ def create_pull_request(organization, repo, branch_name):
         ),
         head=branch_name,
         # default branch could be 'main', 'master', 'prod', or something else
-        base=repo.default_branch,
+        base=gh_repo.default_branch,
     )
 
 
-def set_up_repo(repo):
+def set_up_repo(clone_url, repo_dir):
     """
     Clone the repository and pull the main branch.
 
-    @param repo: the repository to set up
+    @param clone_url: the authenticated URL to the GitHub repo
+    @param repo_dir: the local directory for the repository
+    @return: GitPython Repo instance
     """
-    destination_path = get_repo_path(repo)
-    if not os.path.isdir(destination_path):
-        logging.log(logging.INFO, "Cloning repo...")
-        repo = git.Repo.clone_from(
-            url=get_github_repo_url_with_credentials(repo),
-            to_path=destination_path,
-        )
-    else:
-        logging.log(logging.INFO, "Setting up repo...")
-        repo = git.Repo(destination_path)
-    origin = repo.remotes.origin
-    logging.log(logging.INFO, "Pulling latest code...")
-    origin.pull()
+    logging.log(logging.INFO, "Cloning repo...")
+    local_repo = git.Repo.clone_from(url=clone_url, to_path=repo_dir)
+    return local_repo
 
 
-def is_codeowners_present(repo):
-    """
-    Check whether the repository has a CODEOWNERS file.
-
-    @param repo: the repository in which to check the existence of CODEOWNERS
-    @return: True if a CODEOWNERS file exists, False otherwise
-    """
-    codeowners_path = get_codeowners_path(repo)
-    return codeowners_path.exists() and codeowners_path.is_file()
-
-
-def get_team_mention_map(repo, teams):
+def get_team_mention_map(codeowners_path, teams):
     """
     Map the team slugs to whether they have been mentioned in the CODEOWNERS
     file in any capacity.
 
-    @param repo: the repo to which the CODEOWNERS file being modified belongs
+    @param codeowners_path: the path of the CODEOWNERS file
     @param teams: all the GitHub teams for all Community Teams of a project
     @return: a dictionary of team slugs and their mentions
     """
-    codeowners_path = get_codeowners_path(repo)
     with open(codeowners_path) as codeowners_file:
         contents = codeowners_file.read()
     return {team.slug: mentionified(team.slug) in contents for team in teams}
 
 
-def add_missing_teams(repo, team_mention_map):
+def add_missing_teams(codeowners_path, team_mention_map):
     """
     Add the mention forms for all missing teams in a new line.
 
-    @param repo: the repo to which the CODEOWNERS file being modified belongs
+    @param codeowners_path: the path of the CODEOWNERS file
     @param team_mention_map: the dictionary of team slugs and their mentions
     """
     missing_team_slugs = [
@@ -248,7 +224,6 @@ def add_missing_teams(repo, team_mention_map):
         for team_slug in team_mention_map.keys()
         if not team_mention_map[team_slug]
     ]
-    codeowners_path = get_codeowners_path(repo)
     with open(codeowners_path, "a") as codeowners_file:
         addendum = generate_ideal_codeowners_rule(missing_team_slugs)
         codeowners_file.write(addendum)
@@ -267,42 +242,19 @@ def generate_ideal_codeowners_rule(team_slugs):
     return f"* {combined_team_slugs}"
 
 
-def get_github_repo_url_with_credentials(repo):
+def get_github_repo_url_with_credentials(repo_name):
     """
     Get the HTTPS URL to the repository which has the username and a GitHub
     token for authentication.
 
-    @param repo: the name of the repository
-    @return: the authenticated URL to the repo
+    @param repo_name: the name of the repository
+    @return: the authenticated URL to the GitHub repo
     """
+    github_username, github_token = get_credentials()
     return (
-        f"https://{GITHUB_USERNAME}:{GITHUB_TOKEN}"
-        f"@github.com/{GITHUB_ORGANIZATION}/{repo}.git"
+        f"https://{github_username}:{github_token}"
+        f"@github.com/{GITHUB_ORGANIZATION}/{repo_name}.git"
     )
-
-
-def get_repo_path(repo):
-    """
-    Get the fully qualified absolute path to the repository on the local file
-    system. Repositories are cloned into eponymous directories inside the
-    working directory.
-
-    @param repo: the name of the repository
-    @return: the absolute path to cloned repository on local FS
-    """
-    return WORKING_DIRECTORY.joinpath(repo)
-
-
-def get_codeowners_path(repo):
-    """
-    Get the fully qualified absolute path to the CODEOWNERS file inside the
-    given repository. By convention the file should be located inside the
-    '.github/' directory in the repository.
-
-    @param repo: the name of the repository
-    @return: the absolute path to the CODEOWNERS file
-    """
-    return get_repo_path(repo).joinpath(".github", "CODEOWNERS")
 
 
 def mentionified(team_slug):
