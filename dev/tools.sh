5b7c0fd0567e48909e7bbc2eb0a96828d2a823d7
Timid Robot Zehta <timid@creativecommons.org>
2025-04-24 07:16:07 +0200

    better align script with best and local practices

--- dev/tools.sh
+++ dev/tools.sh
@@ -1,16 +1,54 @@
-#!/bin/bash
+#!/usr/bin/env bash
+#
+# Run Python code tools (isort, black, flake8)
+#
 #### SETUP ####################################################################
+
+set -o errexit
 set -o errtrace
 set -o nounset
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'isort'
+# shellcheck disable=SC2154
+trap '_es=${?};
+    printf "${0}: line ${LINENO}: \"${BASH_COMMAND}\"";
+    printf " exited with a status of ${_es}\n";
+    exit ${_es}' ERR
+
+DIR_REPO="$(cd -P -- "${0%/*}/.." && pwd -P)"
+# https://en.wikipedia.org/wiki/ANSI_escape_code
+E0="$(printf "\e[0m")"        # reset
+E30="$(printf "\e[30m")"      # black foreground
+E31="$(printf "\e[31m")"      # red foreground
+E107="$(printf "\e[107m")"    # bright white background
+
+#### FUNCTIONS ################################################################
+
+error_exit() {
+    # Echo error message and exit with error
+    echo -e "${E31}ERROR:${E0} ${*}" 1>&2
+    exit 1
+}
+
+print_header() {
+    # Print 80 character wide black on white heading with time
+    printf "${E30}${E107}# %-69s$(date '+%T') ${E0}\n" "${@}"
+}
+
+#### MAIN #####################################################################
+
+cd "${DIR_REPO}"
+
+print_header 'isort'
+# shellcheck disable=SC2068
 pipenv run isort ${@:-.}
 echo
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'black'
+print_header 'black'
+# shellcheck disable=SC2068
 pipenv run black ${@:-.}
 echo
 
-printf "\e[1m\e[7m %-80s\e[0m\n" 'flake8'
+print_header 'flake8'
+# shellcheck disable=SC2068
 pipenv run flake8 ${@:-.}
 echo
